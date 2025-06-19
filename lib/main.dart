4670cf01a82f684737503c19134673f0eaaffa56
Bautista-tine <cristinejoybautista544@gmail.com>
2025-06-19 10:28:23 +0800

    today

--- lib/main.dart
+++ lib/main.dart
@@ -7,41 +7,65 @@ void main() {
         title: Text('Bloozom Tutorial'),
         backgroundColor: Colors.purpleAccent[100],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center, // centers the text vertically
-          children: [
-            Text(
-              'halooo',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 30,
-                fontStyle: FontStyle.italic,
-                color: Colors.deepPurple,
-              ),
-            ),
-            Text(
-              'Halooo',
-              style: TextStyle(
-                fontWeight: FontWeight.w600,
-                fontSize: 30,
-                fontStyle: FontStyle.normal,
-                color: Colors.indigo,
-              ),
-            ),
-            Text(
-              'Konnichiwa',
-              style: TextStyle(
-                fontWeight: FontWeight.w500,
-                fontSize: 30,
-                fontStyle: FontStyle.italic,
-                color: Colors.purple,
-              ),
-            ),
-          ],
+      body: Column(
+         children: [
+           Container(
+             margin: EdgeInsets.all(10),
+             child: (
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('gsw2hgd'),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+                  child: (
+                  Text('tgbms')
+                  ),
+                )
+              ],
+            )
+             ),
+           ),
+           Container (
+              margin: EdgeInsets.symmetric(vertical: 10,horizontal: 5 ),
+             child: (
+             Row (
+               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+               children: [
+                 Text('bhvvdh'),
+                 Container(
+                   padding: EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+                   child:(
+                   Text('egduwgd')
+                   ),
+                 )
+               ],
+             )
+             ),
+
+           ),
+           Container(
+             margin: EdgeInsets.fromLTRB(5, 10, 15, 5),
+             child: (
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text('gyfdef'),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+                  child: (
+                  Text('bvvsg')
+                  ),
+                )
+              ],
+            )
+             ),
+           )
+
+         ],
         ),
       ),
     ),
-  ));
+  );
 }
 
